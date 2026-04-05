b480959d2aaa2be1404ffca2b6227795859657d8
theoldman-babe <yakoubinadir74@gmail.com>
2026-04-05 12:18:37 +0100

    Implement priority extraction and insertion functions
    extraire_element_prioirtaire() which returns the process with highes priority, inserer() which insert an element in the right place in a sorted list, and extraire_premier_element() which does the function of pop in a stack (get the 1st element)

--- scheduler.c
+++ scheduler.c
@@ -54,4 +54,69 @@ void free_all_processes(Process *tete){
 }
 }
 
-// Process* extraire_meilleure_priorite(Process **head)
+Process* extraire_meilleure_priorite(Process **tete){
+    if (*tete==NULL){
+    return NULL;}
+
+    Process* actuel=*tete;
+    Process* max=*tete;
+    
+    while (actuel!=NULL){ //on cherche le process avec la priorité la plus haute
+        
+        if (actuel->priority > max->priority){ 
+            max=actuel;
+        }
+        actuel=actuel->suivant;  //on avance après chaque verif
+    }
+    
+    
+    // on recoude les wagons de la liste
+    if (max->precedent!=NULL){
+        max->precedent->suivant=max->suivant;
+}   else{
+    *tete=max->suivant;
+}
+
+if (max->suivant != NULL) {
+        max->suivant->precedent = max->precedent;   
+     }
+    
+max->precedent=NULL; max->suivant=NULL;  // on isole le max pour écraser les ancienne adresse(le processuer va crooire qu'il est tjrs attaché à sa liste)
+return max;        
+}
+
+Process* inserer (Process* tete, Process* nv) { 
+      if (tete == NULL || nv->priority > tete->priority) {
+        nv->suivant = tete; // Cas où la liste est vide ou le nouveau est le plus important
+        if (tete != NULL) {
+            tete->precedent = nv;
+            }nv->precedent = NULL; // le nouveau est le premier, et donc y'a personne à sa gauche;
+            return nv; 
+    }
+    Process* actuel=tete;
+    while(actuel->suivant != NULL && actuel->suivant-> priority >= nv->priority){// On parcourt la liste pour trouver où mettre le nouveau wagon
+        actuel=actuel->suivant;}
+    nv->suivant=actuel->suivant;       //on insère nv entre actuel et le suivant
+    nv->precedent=actuel;
+
+    if (actuel->suivant!=NULL){
+        actuel->suivant->precedent=nv; // si ce n'est pas la fin de la liste en double le chainage entre le suivant et nv
+    }
+    actuel->suivant=nv;  
+return tete;}
+
+Process* extraire_premiere_elt(Process **tete ){
+    
+    if (*tete == NULL) return NULL; // si c'est une liste vide, ça return vide
+
+    Process* cible = *tete;   // on cible la tete
+    *tete=cible->suivant; // on passse un autre wagon (le suivant) à l'adresse de lal tete
+
+    if (cible->suivant != NULL) 
+    {
+        cible->suivant->precedent=NULL; // si ce n'est pas une liste à un elt, je mets à jour le suivant comme tete, donc il a rien à sa gauche
+    }
+    cible->suivant=NULL;cible->precedent=NULL; // j'isole la cible et je la renvoie
+    return cible;
+
+}
